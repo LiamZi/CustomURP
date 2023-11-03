6b46e054f736c53746ce6e15e60467ac75aa419c
LiamZi <yanzi1221@hotmail.com>
2023-11-03 19:32:06 +0800

    1. 增加unsafe 容器

---
+++ Assets/Custom RP/Core/UnsafeHashMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace Core
+{
+    public unsafe partial struct UnsafeHashMap
+    {
+        UnsafeHashCollection _collection;
+        int _valueOffset;
+        
+        
+    }
+}
